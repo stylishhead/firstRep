56985ef9508e56fafb2b961199e60d1a659305fe
stylishhead <145991939+stylishhead@users.noreply.github.com>
2024-01-08 23:08:34 +1000

    3 этап

--- nineSteps/main.cpp
+++ nineSteps/main.cpp
@@ -1,3 +1,4 @@
+#include <ctime>
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -20,7 +21,7 @@ public:
 
     bool isHonors(){ //является ли отличником студент
         if(this->marks.size()==0){
-            return false;
+            return true; //изменил, чтобы при выставлении оценки за счет настроения преподавателя было разнообразие студентов (логика: отличник не тот у кого все оценки 5, а тот у кого нет плохих (ну типа))
         }
         for (int i=0; i<this->marks.size(); i++){
             if(this->marks[i]!=5){
@@ -34,23 +35,37 @@ public:
 class Teacher{
 private:
     string name;
+    bool mood;
+    int random_mark(int x, int y){
+        int random = rand() % 2;
+        random==1? random=x: random=y;
+        return random;
+    }
 public:
     Teacher(): name("Josh"){};
-    Teacher(string name): name(name){};
+    Teacher(string name): name(name), mood(true){};
 
-    void addMarkTo(Student &student, int mark){
-        student.addMark(mark);
+
+    void addMarkTo(Student &student){
+        switch(mood){
+            case true: student.isHonors()? student.addMark(5) : student.addMark(4);
+            case false: student.isHonors()? student.addMark(random_mark(4,5)) : student.addMark(random_mark(2,3));
+        }
     }
 
+    void setMood(bool mood){
+        this->mood = mood;
+    }
 };
 
 int main() {
+    srand(time(0));
     Student Nick = Student("Nick");
     Student Tony = Student("Tony");
 
     Teacher James = Teacher("James");
-    James.addMarkTo(Nick, 5);
-    James.addMarkTo(Tony, 3);
+    James.addMarkTo(Nick);
+    James.addMarkTo(Tony);
     cout << Nick.isHonors() << endl;
     cout << Tony.isHonors() << endl;
     return 0;
