256fa221c8f16dc99d4576b4ef0f66d14bea0a39
stylishhead <145991939+stylishhead@users.noreply.github.com>
2024-01-08 23:09:34 +1000

    5 этап

--- nineSteps/main.cpp
+++ nineSteps/main.cpp
@@ -40,20 +40,39 @@ class Teacher{
 private:
     string name;
     bool mood;
+    int freak = 0; // переменная, отвечающая за то, ставит ли препод только 5 или 2 (==1 -> ставит 5, ==2 -> ставит 2, иначе...)
     int random_mark(int x, int y){
         int random = rand() % 2;
         random==1? random=x: random=y;
         return random;
     }
 public:
-    Teacher(): name("Josh"){teacherList.push_back(*this);};
+    Teacher(): name("Josh"), mood(true){teacherList.push_back(*this);};
     Teacher(string name): name(name), mood(true){teacherList.push_back(*this);};
 
-
     void addMarkTo(Student &student){
-        switch(mood){
-            case true: student.isHonors()? student.addMark(5) : student.addMark(4);
-            case false: student.isHonors()? student.addMark(random_mark(4,5)) : student.addMark(random_mark(2,3));
+        switch (this->freak){
+            case 1: {
+                student.addMark(5);
+                break;
+            }
+            case 2: {
+                student.addMark(2);
+                break;
+            }
+            default: {
+                switch (mood) {
+                    case true: {
+                        student.isHonors() ? student.addMark(5) : student.addMark(4);
+                        break;
+                    }
+                    case false: {
+                        student.isHonors() ? student.addMark(random_mark(4, 5)) : student.addMark(random_mark(2, 3));
+                        break;
+                    }
+                }
+                break;
+            }
         }
     }
 
@@ -64,15 +83,20 @@ public:
     void setMood(bool mood){
         this->mood = mood;
     }
+
+    void setFreak(int fr){
+        this->freak = fr;
+    }
+
 };
 
 class Lesson{
 private:
-    Teacher tchr;
+    Teacher* tchr;
     vector<Student*> comers;
 public:
     void addTeacher(Teacher &teacher){
-        this->tchr = teacher;
+        this->tchr = &teacher;
     }
 
     void addStudent(Student &student){
@@ -84,17 +108,17 @@ public:
             int numberStudents = rand() % (comers.size() + 1) ; //количество студентов, которые получат оценки
             for (int i = 0; i < numberStudents; i++) {
                 int stud = rand() % (comers.size()); //номер студента в списке
-                switch (this->tchr.getMood()) {
+                switch (this->tchr->getMood()) {
                     case true: { //если хорошее настроение, то 1-5 оценок
                         int y = rand() % 5 + 1;
                         for (int j = 0; j < y; j++) {
-                            tchr.addMarkTo(*comers[stud]);
+                            tchr->addMarkTo(*comers[stud]);
                         }
                     }
                     case false: { //если плохое настроение, то 1-2
                         int y = rand() % 2 + 1;
                         for (int j = 0; j < y; j++) {
-                            tchr.addMarkTo(*comers[stud]);
+                            tchr->addMarkTo(*comers[stud]);
                         }
                     }
                 }
@@ -109,11 +133,13 @@ int main() {
     Student Tony = Student("Tony");
 
     Teacher James = Teacher("James");
-
+    James.setMood(false);
+    
     Lesson lesson1;
     lesson1.addStudent(Nick);
     lesson1.addStudent(Tony);
     lesson1.addTeacher(James);
+    James.setFreak(2); //если поставит 1, то ставит только оценку 5, если 2, то только 2, иначе - обычный преподаватель
     lesson1.grading();
     cout << Nick.isHonors() << endl;
     cout << Tony.isHonors() << endl;
